bdef6ca12ef1c1af07202da19442211c6a1d517e
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-06-18 12:17:03 +1200

    Update 02_input_checker.py

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -4,11 +4,21 @@ def user_choice():
     valid = False
     while not valid:
 
-        response = "File type (integer / text / image): ".lower()
+        # Asks user for choice and change response to lowercase
+        response = input("File type (integer / text / image): ").lower()
 
+        # If they choose "t" or "text", return "text"
         if response == "text" or response == "t":
-            return response
+            return "text"
 
         else:
+            # If response is not valid, output an error
             print("Please choose a valid file type!")
             print()
+
+
+# Main routine goes here
+data_type = user_choice()
+print("You chose", data_type)
+
+print()
