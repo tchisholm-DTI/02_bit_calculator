72c5fc42203493b268af18533c6cfa92f38f4fc1
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-06-23 12:57:59 +1200

    Update 00_bit_calc.py

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -16,4 +16,19 @@ def statement_generator(text, decoration):
     return ""
 
 # Main routine goes here
-statement_generator("look - stars", "*")
+
+# Heading
+statement_generator("Bit Calculator for Integers, Text & Images", "-")
+
+# Displays instructions if user has not used the program before
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    # Ask the user for the file type
+
+    # For integers, ask for integer
+    # (must be an integer more than/equal to 0)
+
+    # For images, ask for width and height
