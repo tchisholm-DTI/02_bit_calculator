ba864cec9ec94259e65f0e2dab1039f9a809aeb1
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2021-06-29 14:28:04 +1200

    Update 07_instructions.py

--- 07_instructions.py
+++ 07_instructions.py
@@ -20,9 +20,9 @@ def instructions():
     print()
     print("Please choose a data type (image/text/integer)")
     print()
-    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that ascii encoding is being used (8 bits per character.) ")
+    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that ascii encoding is being used (8 bits per character). ")
     print()
-    print("Complete a many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit")
+    print("Complete as many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit.")
     print()
     return ""
 
